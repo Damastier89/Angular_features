c73f9c893508268fa7c29cc76cfc2491d5e131f6
Sergei <example@mail.ru>
2022-07-28 21:22:50 +0300

    Implemented edit articles

--- src/app/admin/edit-articles/edit-articles.component.ts
+++ src/app/admin/edit-articles/edit-articles.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormGroupDirective, NgForm, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { FormGroupDirective, NgForm, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { ActivatedRoute } from '@angular/router';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
+import { SnackBarTypes } from '../../shared/_models/snack-bar-types.enum';
+import { SnackBarService } from '../../shared/services/snack-bar.service';
+import { ConfirmComponent } from '../../shared/_models/confirm/confirm.component';
 import { Article } from '../shared/interfaces/article';
 import { ArticleService } from '../shared/services/article.service';
 
@@ -24,9 +28,14 @@ export class EditArticlesComponent implements OnInit {
   public form!: UntypedFormGroup;
   public article!: Article;
 
+  private confirmRef!: MatDialogRef<ConfirmComponent>
+
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private articleService: ArticleService,
+    private dialog: MatDialog,
+    private sneckBarService: SnackBarService,
   ) { }
 
   ngOnInit(): void {
@@ -44,6 +53,47 @@ export class EditArticlesComponent implements OnInit {
     });
   }
 
-  public editArticle() {}
+  public editArticle() {
+    this.confirmRef = this.dialog.open(ConfirmComponent, {
+      data: {
+        text: 'Закончить редактирование раздела?',
+        buttons: {
+          confirm: 'Да',
+          cancel: 'Нет'
+        }
+      }
+    });
+
+    this.confirmRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        if (this.form.invalid) {
+          return;
+        }
+
+        this.submitted = true;
+        this.articleService.updateArticle({
+          ...this.article,
+          title: this.form.value.title,
+          content: this.form.value.content,
+          author: this.form.value.author,
+          date: new Date(),
+        }).subscribe(() => {
+          this.submitted = false;
+          this.router.navigate(['/admin', 'dashboard']);
+        });
+
+        this.openSnackBar(SnackBarTypes.Success, 'Раздел отредактирован');
+      } else {
+        this.openSnackBar(SnackBarTypes.Warning, 'Редактирование раздела прервано');
+      }
+    });
+  }
+
+  private openSnackBar(actionType: string, message: string): void {
+    this.sneckBarService.openSnackBar({
+      actionType,
+      message,
+    });
+  }
 
 }
