efb1e9eb94b78c2fe155c0057ab5c7f814b3d53f
Sergei <53522609+Damastier89@users.noreply.github.com>
2022-09-18 13:30:55 +0300

    Delete src/environments directory
    The directory was deleted for security reasons

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-import { Enviroment } from './../app/shared/interfaces/enviroment';
-
-export const environment: Enviroment = {
-  production: false,
-  apiKey: "AIzaSyBPUAfi8bdwfu3dCv6IOy4VMU1VsvDm4qM",
-  fbDbUrl: 'https://agular-features-default-rtdb.firebaseio.com/',
-};
-
-// для теста оставлю это тут //
